b8440054b7e007c9decf9e68cda1985778792e6c
georgibarzev <a581541@MC0QNAWC.ww930.my-it-solutions.net>
2015-07-13 11:45:32 +0100

    new cf account update

--- public/js/controllers/CanopyCtrl.js
+++ public/js/controllers/CanopyCtrl.js
@@ -1,8 +1,9 @@
 angular.module('CanopyCtrl', []).controller('CanopyController', ['$scope', '$location', '$http', '$window', 'cloudfabric', function($scope, $location, $http, $window, cloudfabric) {
 
-	$scope.pressed = function () {
+	$scope.newCFAccount = function () {
 		cloudfabric.get().success(function(data) {
 			alert(data.password);
 		})
 	};
+
 }]);
